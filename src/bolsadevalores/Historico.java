d39dba159cb2f3180138bc71dd297b05f5edb302
leonardosoux <126433909+leonardosoux@users.noreply.github.com>
2023-11-12 16:42:20 -0300

    Update Historico.java

--- src/bolsadevalores/Historico.java
+++ src/bolsadevalores/Historico.java
@@ -1,5 +1,18 @@
 package bolsadevalores;
 
+import bolsadevalores.estruturas.Pilha;
+
 public class Historico {
+    private Pilha<Transacao> historico;
+
+    public Historico(){
+        this.historico = new Pilha<>();
+    }
+
+    public void addTransacao(Transacao transacao) {
+        historico.empilhar(transacao);
+    }
+
+}
     
 }
