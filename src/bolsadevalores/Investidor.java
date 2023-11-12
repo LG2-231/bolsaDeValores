70fedaa001568de0c433ee042a502589e7754674
leonardosoux <126433909+leonardosoux@users.noreply.github.com>
2023-11-12 16:45:39 -0300

    Update Investidor.java

--- src/bolsadevalores/Investidor.java
+++ src/bolsadevalores/Investidor.java
@@ -3,31 +3,19 @@ package bolsadevalores;
 import java.util.ArrayList;
 import java.util.List;
 
-import bolsadevalores.estruturas.Pilha;
-
 public class Investidor {
     private String nome;
     private String cpf;
     private List<Carteira> carteiras;
-    private Pilha<Ordem> historicoOrdens;
 
     public Investidor (String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
         this.carteiras = new ArrayList<>();
-        this.historicoOrdens = new Pilha<>();
     }
 
     public void adicionarCarteira(Carteira carteira) {
         carteiras.add(carteira);
-    }
-
-    public void enviarOrdem(Ordem ordem) {
-        historicoOrdens.empilhar(ordem);
-    }
-    
-    public Ordem desfazerOrdem() {
-        return historicoOrdens.desempilhar();
     }
 
     public String getNome() {
